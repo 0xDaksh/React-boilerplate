2ae9fa230ad8185b714bd23ebad31aab2dc08d02
Daksh Miglani <hello@dak.sh>
2017-12-18 22:57:32 +0530

    ooh yeah router ad ded.

--- src/view/App.js
+++ src/view/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Home from './routes/home'
+import About from './routes/about'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2 style={{textAlign: 'center'}}>Yolo, this is the entry point.</h2>
+				<BrowserRouter>
+					<div>
+						<Route path="/" exact component={Home} />
+						<Route path="/about" component={About} />
+						<Link to='/'>
+							Home Page.
+						</Link>
+						<Link to='/about'>
+							About Page.
+						</Link>
+					</div>
+				</BrowserRouter>
 			</div>
 		)
 	}
