efa20bd17231debe9dfed44a7bd75d1675103bda
Daksh Miglani <hello@dak.sh>
2017-12-19 16:56:17 +0530

    cleaner

--- src/view/App.js
+++ src/view/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import Home from './routes/home'
-import About from './routes/about'
+import Home from './routes/Home'
 
 export default class App extends React.Component {
 	render() {
@@ -10,13 +9,6 @@ export default class App extends React.Component {
 				<BrowserRouter>
 					<div>
 						<Route path="/" exact component={Home} />
-						<Route path="/about" component={About} />
-						<Link to='/'>
-							Home Page.
-						</Link>
-						<Link to='/about'>
-							About Page.
-						</Link>
 					</div>
 				</BrowserRouter>
 			</div>
