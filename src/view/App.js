d41fbbb6c5a19d219544dc52007431cf3a06f0f3
Daksh Miglani <hello@dak.sh>
2017-12-19 16:58:22 +0530

    normalize it :)

--- src/view/App.js
+++ src/view/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Home from './routes/Home'
+import 'normalize.css'
 
 export default class App extends React.Component {
 	render() {
