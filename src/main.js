7a59690a5331fd1ca307f3e2a2df6776f38ff709
Daksh Miglani <hello@dak.sh>
2017-12-22 21:08:05 +0530

    service worker

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './view/App'
+import injectServiceWorker from './injectSW'
 
 ReactDOM.render(<App />, document.getElementById('app'))
+injectServiceWorker()
