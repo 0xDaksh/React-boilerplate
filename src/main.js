bc2237ad1f52c1de04cfdaea77dc47dce8659a56
Daksh Miglani <hello@dak.sh>
2017-12-22 21:21:22 +0530

    fix it by adding cool stuff

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './view/App'
-import injectServiceWorker from './injectSW'
-
-ReactDOM.render(<App />, document.getElementById('app'))
-injectServiceWorker()
