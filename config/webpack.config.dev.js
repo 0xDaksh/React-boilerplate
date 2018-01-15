72a5abfabca2e051bbf9fcf39585fb0e559bcb27
Daksh Miglani <hello@dak.sh>
2018-01-15 11:17:09 +0530

    added jarvis

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -11,6 +11,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
+const Jarvis = require('webpack-jarvis');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -211,6 +212,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new Jarvis(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
